f41bc1f60a6f2d5b7fc3a0d0771dc6d817dac18c
TEJADA ARAGON EMILIANO <49380010@ort>
2024-09-13 14:53:23 -0300

    main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+import cv2
+import time
+import requests as req
+import json
+
+def sendRequest(data):
+    payload={
+        "TOKEN":data,
+        "CARRO_ID":7
+    }
+    try:        
+        res=req.put("https://secure-track-db.vercel.app/computers/withdrawal",json=payload, headers={'content-type': 'application/json'})
+        if res.status_code==200:
+            return res.json()
+        else:
+            return res.status_code
+    except Exception as e:
+        return(e) 
+
+capture = cv2.VideoCapture(0)
+qrDetector = cv2.QRCodeDetector()
+while capture.isOpened():
+    ret, frame = capture.read()
+    cv2.imshow("webcam", frame)
+    if cv2.waitKey(1) == ord("q"):
+        break
+    data, bbox, rectifiedImage = qrDetector.detectAndDecode(frame)
+    if len(data) > 0:
+        print(f"QR Code detected: {data}")
+        print(sendRequest(data))
+        time.sleep(2)
+    time.sleep(0.002)
+capture.release()
+cv2.destroyAllWindows()
