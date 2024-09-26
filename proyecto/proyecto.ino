36305844ca1a52e994f680ff4110c78dcdd3508d
TEJADA ARAGON EMILIANO <49380010@ort>
2024-09-26 10:14:36 -0300

    arduino update - retiro y devoluciones

--- proyecto/proyecto.ino
+++ proyecto/proyecto.ino
@@ -2,11 +2,11 @@
 #include <List.hpp>
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27,20,4);
-Servo servoSlot1;
-Servo servoSlot2;
-Servo servoSlot3;
-Servo servoSlot4;
+#include<SPI.h>
+#include<MFRC522.h>
+
+#define RFID_RST_PIN 49
+#define RFID_SS_PIN 53
 #define maxValores 5
 #define ledSlot1 34
 #define ledSlot2 36
@@ -20,8 +20,17 @@ Servo servoSlot4;
 #define btnGral2 24
 #define buzzer 48
 #define electroIman 41
+
+LiquidCrystal_I2C lcd(0x27,20,4);
+MFRC522 rfid(RFID_SS_PIN,RFID_RST_PIN);
+Servo servoSlot1;
+Servo servoSlot2;
+Servo servoSlot3;
+Servo servoSlot4;
+
 List<String> inputList;
 int timeStart;
+String uidString="";
 
 void setup() {
   // put your setup code here, to run once:
@@ -30,6 +39,8 @@ void setup() {
   lcd.begin(16,2);
   lcd.backlight();
   lcd.setCursor(0,0);
+  SPI.begin();
+  rfid.PCD_Init();
   servoSlot1.attach(2);
   servoSlot2.attach(3);
   servoSlot3.attach(4);
@@ -154,6 +165,8 @@ void loop() {
                   digitalWrite(buzzer, LOW);
                 }
               }
+              general(false);
+              digitalWrite(buzzer, LOW);
               lcd.clear();
               break;
             case 2:
@@ -172,6 +185,8 @@ void loop() {
                   digitalWrite(buzzer, LOW);
                 }
               }
+              general(false);
+              digitalWrite(buzzer, LOW);
               lcd.clear();
               break;
             case 3:
@@ -190,6 +205,8 @@ void loop() {
                   digitalWrite(buzzer, LOW);
                 }
               }
+              general(false);
+              digitalWrite(buzzer, LOW);
               lcd.clear();
               break;
             case 4:
@@ -208,13 +225,112 @@ void loop() {
                   digitalWrite(buzzer, LOW);
                 }
               }
+              general(false);
+              digitalWrite(buzzer, LOW);
               lcd.clear();
               break;
           }
-      }else if(inputList[0]=="retMultiple"){
+      }else if(inputList[0]=="2"){ // retiro
+        inputList.remove(0);
+        if(inputList[0].toInt()==1){
+          slot1(true);
+        }if(inputList[1].toInt()==2){
+          slot2(true);
+        }if(inputList[2].toInt()==3){
+          slot3(true);
+        }if(inputList[3].toInt()==4){
+          slot4(true);
+        }
+        timeStart=millis();
+        while(!((digitalRead(btnSlot1)==LOW || inputList[0].toInt()==0) && (digitalRead(btnSlot2)==LOW || inputList[1].toInt()==0) && (digitalRead(btnSlot3)==LOW || inputList[2].toInt()==0) && (digitalRead(btnSlot4)==LOW || inputList[3].toInt()==0))){
+          
+        }
+        digitalWrite(buzzer, LOW);
+        slot1(false);
+        slot2(false);
+        slot3(false);
+        slot4(false);
+        while(digitalRead(btnGral1) || digitalRead(btnGral2)){
+          lcd.clear();
+          lcd.print("cerrar la puerta");
+          if(timeStart+120000<=millis()){
+            digitalWrite(buzzer, HIGH);
+          }else{
+            digitalWrite(buzzer, LOW);
+          }
+        }
+        lcd.clear();
+      }else if(inputList[0]=="1"){ // devolucion
+        lcd.clear();
+        lcd.print("acerque la computadora al sensor");
+        if(rfid.PICC_IsNewCardPresent()){
+        if(rfid.PICC_ReadCardSerial()){
+          String uidString="";
+          for (byte i = 0; i < rfid.uid.size; i++) {
+            uidString += String(rfid.uid.uidByte[i] < 0x10 ? "0" : "");
+            uidString += String(rfid.uid.uidByte[i], HEX);
+          }
+          for(int i=0; i<inputList.getSize();i++){
+            if(inputList[i]==uidString){
+              switch(i+1){
+                case 1:
+                  slot1(true);
+                  lcd.clear();
+                  lcd.print("guardar en slot 1");
+                  while(digitalRead(btnSlot1)){
 
-      }else if(inputList[0]=="devMultiple"){
+                  }
+                  slot1(false);
+                  lcd.clear();
+                  break;
+                case 2:
+                  slot2(true);
+                  lcd.clear();
+                  lcd.print("guardar en slot 2");
+                  while(digitalRead(btnSlot2)){
 
+                  }
+                  slot2(false);
+                  lcd.clear();
+                  break;
+                case 3:
+                  slot3(true);
+                  lcd.clear();
+                  lcd.print("guardar en slot 3");
+                  while(digitalRead(btnSlot3)){
+
+                  }
+                  slot3(false);
+                  lcd.clear();
+                  break;
+                case 4:
+                  slot4(true);
+                  lcd.clear();
+                  lcd.print("guardar en slot 4");
+                  while(digitalRead(btnSlot4)){
+
+                  }
+                  slot4(false);
+                  lcd.clear();
+                  break;
+              }
+              
+            }
+          }
+          lcd.clear();
+          lcd.print("cerrar la puerta porfavor");
+          timeStart=millis();
+          while(digitalRead(btnGral1) && digitalRead(btnGral2)){
+            if(timeStart+120000==millis()){
+              digitalWrite(buzzer, HIGH);
+            }else{
+              digitalWrite(buzzer, LOW);
+            }
+          }
+          digitalWrite(buzzer, LOW);
+          lcd.clear();
+        }
+  }
       }else{
         lcd.clear();
         lcd.print("error");
@@ -223,10 +339,11 @@ void loop() {
           delay(250);
           digitalWrite(buzzer, LOW);
           delay(500);
-        }
         digitalWrite(buzzer, LOW);
         lcd.clear();
+        }
       }
     }
   }
+  lcd.clear();
 }
