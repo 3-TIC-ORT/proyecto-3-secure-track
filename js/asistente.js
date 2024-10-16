f87b9a7050afcd4851b03b947fe4131b16e302d8
Brigitte Blau <brigitteyaelblau@gmail.com>
2024-10-16 15:56:52 -0300

    anda el search

--- js/asistente.js
+++ js/asistente.js
@@ -1,6 +1,8 @@
     // if (!sessionStorage.getItem("asist-Key")) {
         //     window.location.href = "accesodenegado.html";
         // 
+        const tbody = document.querySelector('#transaccionesTable tbody');
+
 let go = document.getElementById("go")
 let input = document.getElementById("input")
 go.addEventListener("click", ()=>filtrarTransacciones())
@@ -19,32 +21,59 @@ async function cargarTransacciones() {
         console.log(transacciones)
         
    
-        const tbody = document.querySelector('#transaccionesTable tbody');
         
         
         tbody.innerHTML = '';
         
        
         transacciones.tokens.forEach(transaccion => {
-            const fila = document.createElement('tr');
-            
-            fila.innerHTML = `
-                <td>${transaccion.token.user.username}</td>
-                <td>${transaccion.token.user.occupation}</td>
-                <td>${transaccion.token.cart.room.roomNumber}</td>
-                <td>${transaccion.computerId}</td>
-                <td>${transaccion.token.status}</td>
-                <td>${transaccion.token.createdAt.toString()}</td>
-            `;
-            
-            tbody.appendChild(fila);
+          crearTransacciones(transaccion)
         });
     } catch (error) {
        console.log(error)
     }
 }
 
-async function filtrarTransacciones() {
+cargarTransacciones();
+ function crearTransacciones(data) {
+
+    const fila = document.createElement('tr');
+            
+    fila.innerHTML = `
+        <td>${data.token.user.username}</td>
+        <td>${data.token.user.occupation}</td>
+        <td>${data.token.cart.room.roomNumber}</td>
+        <td>${data.computerId}</td>
+        <td>${data.token.status}</td>
+        <td>${data.token.createdAt.toString()}</td>
+    `;
+    
+    tbody.appendChild(fila);
+}
+
+const alumno= document.getElementById("alumno")
+const ocupacion = document.getElementById("ocupacion")
+const computadora= document.getElementById("aula")
+const estado = document.getElementById("estado")
+const horario = document.getElementById("horario")
+const buscar = document.getElementById("go")
+
+// || ocupacion.value.length > 0 || aula.value.length > 0 || estado.value.length > 0 || horario.value.length > 0 
+alumno.addEventListener("change",()=>{
+    
+        tb.innerHTML=""
+        ocupacion.setAttribute("disabled",true)  
+})
+
+
+
+
+
+
+buscar.addEventListener("click", filtrado)
+
+
+async function filtrado() {
     try {
        
         const response = await fetch('https://secure-track-db.vercel.app/asistente/tokens/filtrados',{
@@ -54,20 +83,21 @@ async function filtrarTransacciones() {
                 "Content-Type": "application/json",
             },
             body:JSON.stringify({
-                request: input.value
+                type:"alumno",
+                data:alumno.value
             })
            
          }); 
-        const transacciones = await response.json();
-        console.log(transacciones)
-        
-   
+        const hola  = await response.json();
+        console.log(hola)
+        tbody.innerHTML = '';
+    hola.data.forEach(element => {
+        crearTransacciones(element)
+    });
         
     } catch (error) {
        console.log(error)
     }
 }
 
-
-
-cargarTransacciones();
+//solo escribir en ub 
