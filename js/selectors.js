16422c56565be717e2492816fa2a88a40dd98a9b
Brigitte Blau <brigitteyaelblau@gmail.com>
2024-10-29 15:44:34 -0300

    Refactor CSS to update button styles, adjust font sizes, and optimize code

--- js/selectors.js
+++ js/selectors.js
@@ -93,7 +93,7 @@ async function updateClassroomsOptions(piso, edificio) {
         options.forEach(room => {
             let opt = document.createElement("option");
             opt.value = room.id;
-            // opt.textContent = room.room.roomNumber ;
+            // opt.textContent = room.room.roomNumber;
             opt.textContent = `ID: ${room.id} - Aula: ${room.room.roomNumber}`; 
             classrooms.appendChild(opt);
         });
