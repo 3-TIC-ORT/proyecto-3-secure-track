5830cf2688651cccc3ea32e4c1e060761e82d2fa
TEJADA ARAGON EMILIANO <49380010@ort>
2025-07-17 10:04:23 -0300

    requirements.txt

--- Scripts/read_zbar.py
+++ Scripts/read_zbar.py
@@ -1,4 +1,4 @@
-#!C:\Users\sabri\Documents\proyecto-3-secure-track\Scripts\python.exe
+#!C:\Users\49380010\Desktop\proyecto-3-secure-track\Scripts\python.exe
 from __future__ import print_function
 
 import argparse
